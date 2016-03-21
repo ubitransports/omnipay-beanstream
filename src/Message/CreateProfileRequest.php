ac84e1c551cf003712f119cb603fcbf4552ab5e0
Andy Perdomo <andres.a.perdomo@gmail.com>
2016-03-21 10:22:03 -0700

    Cleanup in CreateProfileRequest.

--- src/Message/CreateProfileRequest.php
+++ src/Message/CreateProfileRequest.php
@@ -8,18 +8,7 @@ class CreateProfileRequest extends AbstractProfileRequest
 
         $data = array(
             'language' => $this->getLanguage(),
-            'comment' => $this->getComments(),
-            'billing' => array(
-                'name' => $this->getCard()->getBillingName(),
-                'address_line1' => $this->getCard()->getBillingAddress1(),
-                'address_line2' => $this->getCard()->getBillingAddress2(),
-                'city' => $this->getCard()->getBillingCity(),
-                'province' => $this->getCard()->getBillingState(),
-                'country' => $this->getCard()->getBillingCountry(),
-                'postal_code' => $this->getCard()->getBillingPostcode(),
-                'phone_number' => $this->getCard()->getBillingPhone(),
-                'email_address' => $this->getCard()->getEmail(),
-            )
+            'comment' => $this->getComments()
         );
 
         if ($this->getCard()) {
@@ -29,6 +18,18 @@ class CreateProfileRequest extends AbstractProfileRequest
                 'expiry_month' => $this->getCard()->getExpiryDate('m'),
                 'expiry_year' => $this->getCard()->getExpiryDate('y'),
                 'cvd' => $this->getCard()->getCvv(),
+            );
+
+            $data['billing'] => array(
+                'name' => $this->getCard()->getBillingName(),
+                'address_line1' => $this->getCard()->getBillingAddress1(),
+                'address_line2' => $this->getCard()->getBillingAddress2(),
+                'city' => $this->getCard()->getBillingCity(),
+                'province' => $this->getCard()->getBillingState(),
+                'country' => $this->getCard()->getBillingCountry(),
+                'postal_code' => $this->getCard()->getBillingPostcode(),
+                'phone_number' => $this->getCard()->getBillingPhone(),
+                'email_address' => $this->getCard()->getEmail(),
             );
         }
 
