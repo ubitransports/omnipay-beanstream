313471416d1d35e2b3646ed6fd37902ad3baa1a1
Andy Perdomo <andres.a.perdomo@gmail.com>
2016-03-24 12:20:06 -0700

    Adding getters to Response object, Adding request send/faulre tests for authorize, purchase, adding tests for Response.

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -12,17 +12,32 @@ class Response extends AbstractResponse
 
     public function getTransactionReference()
     {
-        return (isset($this->data['id'])) ? $this->data['id'] : null;
+        return isset($this->data['id']) ? $this->data['id'] : null;
+    }
+
+    public function getType()
+    {
+        return isset($this->data['type']) ? $this->data['type'] : null;
     }
 
     public function getOrderNumber()
     {
-        return (isset($this->data['order_number'])) ? $this->data['order_number'] : null;
+        return isset($this->data['order_number']) ? $this->data['order_number'] : null;
+    }
+
+    public function getMessageId()
+    {
+        return isset($this->data['message_id']) ? $this->data['message_id'] : null;
     }
 
     public function getMessage()
     {
-        return isset($this->data['message_id']) ? $this->data['message_id'] : null;
+        return isset($this->data['message']) ? $this->data['message'] : null;
+    }
+
+    public function getAuthCode()
+    {
+        return isset($this->data['auth_code']) ? $this->data['auth_code'] : null;
     }
 
     public function getCode()
@@ -33,5 +48,15 @@ class Response extends AbstractResponse
     public function getCard()
     {
         return isset($this->data['card']) ? $this->data['card'] : null;
+    }
+
+    public function getReference()
+    {
+        return isset($this->data['reference']) ? $this->data['reference'] : null;
+    }
+
+    public function getCategory()
+    {
+        return isset($this->data['category']) ? $this->data['category'] : null;
     }
 }
--- tests/Message/AuthorizeRequestTest.php
+++ tests/Message/AuthorizeRequestTest.php
@@ -17,6 +17,47 @@ class AuthorizeRequestTest extends TestCase
         $this->assertSame('https://www.beanstream.com/api/v1/payments', $this->request->getEndpoint());
     }
 
+    public function testSendSuccess()
+    {
+        $this->request->setAmount('10.00');
+        $this->setMockHttpResponse('AuthorizeSuccess.txt');
+        $response = $this->request->send();
+        $this->assertTrue($response->isSuccessful());
+        $this->assertSame('1000001', $response->getTransactionReference());
+        $this->assertSame('1', $response->getOrderNumber());
+        $this->assertSame('Approved', $response->getMessage());
+        $this->assertSame('1', $response->getMessageId());
+        $this->assertSame('TEST', $response->getAuthCode());
+        $this->assertSame('PA', $response->getType());
+        $this->assertNull($response->getCode());
+        $responseCard = $response->getCard();
+        $this->assertNotEmpty($responseCard);
+        $this->assertSame('VI', $responseCard['card_type']);
+        $this->assertSame('1234', $responseCard['last_four']);
+        $this->assertSame(0, $responseCard['cvd_match']);
+        $this->assertSame(0, $responseCard['address_match']);
+        $this->assertSame(0, $responseCard['postal_result']);
+    }
+
+    public function testSendError()
+    {
+        $this->request->setAmount('10.00');
+        $this->setMockHttpResponse('AuthorizeFailure.txt');
+        $response = $this->request->send();
+        $this->assertSame(49, $response->getCode());
+        $this->assertSame(3, $response->getCategory());
+        $this->assertSame('Invalid transaction request string', $response->getMessage());
+        $this->assertSame('https://www.beanstream.com/docs/errors#49', $response->getReference());
+        $this->assertFalse($response->isSuccessful());
+        $this->assertNull($response->getTransactionReference());
+        $this->assertNull($response->getOrderNumber());
+        $this->assertNull($response->getType());
+        $this->assertNull($response->getMessageId());
+        $this->assertNull($response->getAuthCode());
+        $responseCard = $response->getCard();
+        $this->assertEmpty($responseCard);
+    }
+
     public function testMerchantId()
     {
         $this->assertSame($this->request, $this->request->setMerchantId('123'));
